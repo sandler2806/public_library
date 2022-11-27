1e6e0a0a82026240495cfcb9801cf4ebadcd2552
noamv2 <74304423+noamv2@users.noreply.github.com>
2022-11-27 22:10:05 +0200

    book list added

--- app/src/main/java/com/example/mylib/BorrowBook.java
+++ app/src/main/java/com/example/mylib/BorrowBook.java
@@ -2,13 +2,25 @@ package com.example.mylib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class BorrowBook extends AppCompatActivity {
 
+    ListView bookList;
+    String[] books;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrow_book);
+
+        Resources res = getResources();
+        bookList = (ListView) findViewById(R.id.bookList);
+        books = res.getStringArray(R.array.books);
+
+        bookList.setAdapter(new ArrayAdapter<String>(this, R.layout.book_list_detail,books));
     }
+
 }
