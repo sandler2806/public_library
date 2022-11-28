82182e0edc9251d71d56a1a3f0a7c3ebbef07b3e
Yuval sandler <sandler2806@gmai.com>
2022-11-28 20:55:23 +0200

    delete firebase

--- app/src/main/java/com/example/mylib/MainActivity.java
+++ app/src/main/java/com/example/mylib/MainActivity.java
@@ -9,13 +9,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 
 public class MainActivity extends AppCompatActivity {
-//    FirebaseDatabase database = FirebaseDatabase.getInstance();
-//    public DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
