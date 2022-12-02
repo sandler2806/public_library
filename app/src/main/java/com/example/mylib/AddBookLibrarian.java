f56bb7188a83e3284041aa4b03d9c4d91ee0d6f9
Ido Bar <ido1403@gmail.com>
2022-12-02 12:19:50 +0200

    fireBase read

--- app/src/main/java/com/example/mylib/AddBookLibrarian.java
+++ app/src/main/java/com/example/mylib/AddBookLibrarian.java
@@ -1,14 +1,46 @@
 package com.example.mylib;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddBookLibrarian extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://public-library-8027f-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().getDatabase();
+    DatabaseReference myRef ;
 
+    public void setMyRef(DatabaseReference myRef) {
+        this.myRef = myRef;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book_librarian);
+        ReadBook("1");
+    }
+
+    private void ReadBook(String Id){
+        setMyRef(database.getReference("books"));
+        this.myRef.child(Id).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    DataSnapshot snapshot = task.getResult();
+                    String bookName = String.valueOf(snapshot.child("name").getValue());
+                    Toast.makeText(AddBookLibrarian.this, bookName, Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(AddBookLibrarian.this,"No data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
