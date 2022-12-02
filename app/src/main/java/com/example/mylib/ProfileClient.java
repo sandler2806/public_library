cce2552ce166edce6e24b92e3d59cba4b7965954
ShauliTaragin <shaulitaragin@gmail.com>
2022-12-02 12:06:42 +0200

    Added pages from client home and buttons to and from them

--- app/src/main/java/com/example/mylib/ProfileClient.java
+++ app/src/main/java/com/example/mylib/ProfileClient.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class BorrowBook extends AppCompatActivity {
+public class ProfileClient extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_borrow_book);
+        setContentView(R.layout.activity_profile_client);
     }
     public void goBack(View view){
 
