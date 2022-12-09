01fade9864aafb474243d6ddb1da304ac4d146b8
noamv2 <74304423+noamv2@users.noreply.github.com>
2022-12-09 10:13:14 +0200

    book list redesign, added utillity class, borrow refresh page

--- app/src/main/java/com/example/mylib/BorrowBook.java
+++ app/src/main/java/com/example/mylib/BorrowBook.java
@@ -7,6 +7,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,21 +24,10 @@ import java.util.ArrayList;
 
 
 public class BorrowBook extends AppCompatActivity {
-//    Client a=new Client();
 
-//    Client.name="sdfsd";
     ListView bookList;
-    String[] books;
-    String[] authors;
-    int[] noOfCopies;
-    ListView listView;
+    BookAdapter adapter;
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance("https://public-library-8027f-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().getDatabase();
-    DatabaseReference myRef ;
-
-    public void setMyRef(DatabaseReference myRef) {
-        this.myRef = myRef;
-    }
     public void goBack(View view){
 
         startActivity(new Intent(this, ClientHomeActivity.class));
@@ -48,16 +38,8 @@ public class BorrowBook extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrow_book);
 
-
-
-
-//        Resources res = getResources();
         bookList = (ListView) findViewById(R.id.bookList);
-//        books = res.getStringArray(R.array.books);
-//        authors = res.getStringArray(R.array.authors);
-//        noOfCopies = res.getIntArray(R.array.numberOfCopies);
 
-//        Book(String name, String author, String genre, String publishing_year,int amount)
         Book[] books = new Book[7];
         books[0] = new Book("Harry potter 1", "J.K rolling","", "",1 );
         books[1] = new Book("Harry potter 2", "J.K rolling","", "",1 );
@@ -66,15 +48,18 @@ public class BorrowBook extends AppCompatActivity {
         books[4] = new Book("Harry potter 5", "J.K rolling","", "",1 );
         books[5] = new Book("Harry potter 6", "J.K rolling","", "",1 );
         books[6] = new Book("Harry potter 7", "J.K rolling","", "",1 );
-        BookAdapter adapter = new BookAdapter(this, books);
+        adapter = new BookAdapter(this, books);
         bookList.setAdapter(adapter);
-
 
 
     }
 
     public void borrow(View view){
-        // implement borrowing
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
+
     }
 
 }
