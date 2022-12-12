ee0da2f51ad60a5eda632b771584931b61029a1a
Yuval sandler <sandler2806@gmai.com>
2022-12-12 22:06:59 +0200

    try change listener

--- app/src/main/java/com/example/mylib/GlobalUserInfo.java
+++ app/src/main/java/com/example/mylib/GlobalUserInfo.java
@@ -2,5 +2,7 @@ package com.example.mylib;
 
 public class GlobalUserInfo {
     public static String global_user_name;
+    public static String global_name;
+    public static String global_phone_number;
     public static int global_user_id=0;
 }
