502f172da3c4c4e75c758cda2fe0f60bf08a18f6
Ido Bar <ido1403@gmail.com>
2022-12-18 22:16:10 +0200

    fixed bug

--- app/src/main/java/com/example/mylib/ProfileClient.java
+++ app/src/main/java/com/example/mylib/ProfileClient.java
@@ -17,12 +17,12 @@ public class ProfileClient extends AppCompatActivity {
         textViewToChange.setText(nameEntry);
         //show user name
         String userNameEntry = "Your user name: " + GlobalUserInfo.global_user_name;
-        final TextView textViewToChange2 = (TextView) findViewById(R.id.ClientuserNameInInfoPage);
-        textViewToChange2.setText(userNameEntry);
+//        final TextView textViewToChange2 = (TextView) findViewById(R.id.ClientuserNameInInfoPage);
+//        textViewToChange2.setText(userNameEntry);
         //show user phone number
         String PhoneNumber = "Your phone number: " + GlobalUserInfo.global_phone_number;
-        final TextView textViewToChange3 = (TextView) findViewById(R.id.ClientPhoneNumberInInfoPage);
-        textViewToChange3.setText(PhoneNumber);
+//        final TextView textViewToChange3 = (TextView) findViewById(R.id.ClientPhoneNumberInInfoPage);
+//        textViewToChange3.setText(PhoneNumber);
 
     }
     public void goBack(View view){
