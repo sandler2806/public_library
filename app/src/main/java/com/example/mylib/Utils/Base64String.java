470d1bf07e255efeb18e95932da29a936fc28615
noamv2 <74304423+noamv2@users.noreply.github.com>
2022-12-30 13:16:56 +0200

    Password now saved and read as hashes

--- app/src/main/java/com/example/mylib/Utils/Base64String.java
+++ app/src/main/java/com/example/mylib/Utils/Base64String.java
@@ -3,12 +3,12 @@ import java.util.Base64;
 
 public class Base64String {
 
-    static String encode(byte[] hash_array)
+    public static String encode(byte[] hash_array)
     {
         return Base64.getEncoder().encodeToString(hash_array);
     }
 
-    static byte[] decode(String hash_string)
+    public static byte[] decode(String hash_string)
     {
         return Base64.getDecoder().decode(hash_string);
     }
